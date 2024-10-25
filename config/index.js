51c0154c0d1e98340f1de194b8dc7960238dc087
Janith K. Gunathilake <janithkavinda49@gmail.com>
2024-10-25 11:42:11 +0530

    url

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-  allowedDomains: ['http://aivoicebot.brandcorridor.lk/'] // Add your frontend URL
+  allowedDomains: ['https://aivoicebotapi.brandcorridor.lk/'] // Add your frontend URL
 };
